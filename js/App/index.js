3e3bc1e7880debaf1522220c46a0ecd8840432af
AlexandrHeroCoud <polkanov.freelancer@gmail.com>
2020-09-18 14:17:19 +0300

    fix start of index.html https://docs.google.com/document/d/1XED0beXWx2Rz3hbTcJyDJvxaytfA8mDVyNcbpoRrdNU/edit#heading=h.9nbz79q6ined

--- js/App/index.js
+++ js/App/index.js
@@ -1,4 +1,121 @@
-import {getListBooks, removeBookByIdAPI, getBookByIdApi, setBookByIdAPI, addBookByIdAPI} from './apiLocalStorage.js'
+
+
+////////////////////////////////////////----------INITIALAPP.JS-------------////////////////////////////////////////
+const initialState = ()=>{
+    const arrBooks = [
+        {
+            id:1,
+            author: "Joseph Brodsky",
+            bookTitle: "The collection of poems",
+            pubDate: "1996-08-24",
+            countPages: "201",
+            isBook:true,
+        },{
+            id:2,
+            author: "Ivan Bunin",
+            bookTitle: "The collection of poems 2",
+            pubDate: "1996-08-24",
+            countPages: "201",
+            isBook:true,
+        },{
+            id:3,
+            author: "Mikhail Bulgakov",
+            bookTitle: "The master and Margarita",
+            pubDate: "1996-08-24",
+            countPages: "201",
+            isBook:true,
+        },{
+            id:4,
+            author: "Omar Khayyam",
+            bookTitle: "The collection of poems",
+            pubDate: "1996-08-24",
+            countPages: "201",
+            isBook:true,
+        },{
+            id:5,
+            author: "Mikhail Bulgakov",
+            bookTitle: "Morphine",
+            pubDate: "1996-08-24",
+            countPages: "201",
+            isBook:true,
+        },{
+            id:6,
+            author: "Alexander Pushkin",
+            bookTitle: "The collection of poems",
+            pubDate: "1996-08-24",
+            countPages: "201",
+            isBook:true,
+        },{
+            id:7,
+            author: "Mikhail Sholokhov",
+            bookTitle: "Quiet Don",
+            pubDate: "1996-08-24",
+            countPages: "201",
+            isBook:true,
+        },{
+            id:8,
+            author: "Joseph Brodsky",
+            bookTitle: "The collection of poems",
+            pubDate: "1996-08-24",
+            countPages: "201",
+            isBook:true,
+        }
+    ]
+    for (let i=0; i<arrBooks.length; i++){
+        localStorage.setItem(arrBooks[i].id, JSON.stringify(arrBooks[i]))
+    }
+}
+const getCookie = (name) =>{
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+/**
+ * @function session()
+ * @param {function} initialState
+ * @description проверяем первый вход на страницу
+ * **/
+const session = (initialState, getCookie) =>{
+    if (!getCookie('userSession')){
+        initialState();
+        document.cookie = "userSession=true";
+    }
+}
+session(initialState,getCookie);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////API-LOCALSTORAGE.JS/////////////////////////////////////////////////////////
+//import {getListBooks, removeBookByIdAPI, getBookByIdApi, setBookByIdAPI, addBookByIdAPI} from './apiLocalStorage.js'
+const getListBooks = () =>{
+    const books = [];
+    let key;
+    for (key in localStorage) {
+        const storageObject = JSON.parse(localStorage.getItem(key))
+        if (storageObject!=null){
+            if(storageObject.isBook){
+                books.push(JSON.parse(localStorage.getItem(key)))
+            }
+        }
+    }
+    return books
+}
+
+const getBookByIdApi = (id) =>{
+    return localStorage.getItem(id)
+}
+
+const removeBookByIdAPI = async (id) =>{
+    localStorage.removeItem(id)
+}
+const setBookByIdAPI = async (id, newData) =>{
+    return localStorage.setItem(id, newData)
+}
+const addBookByIdAPI = async (data) =>{
+    localStorage.setItem(data.id, JSON.stringify(data))
+}
+//////////////////////////////////////////////////////////////////////////////////////
 //Please take me on work! )))
 $(document).ready(() => {
     const elemBook = (dataBook) =>{
